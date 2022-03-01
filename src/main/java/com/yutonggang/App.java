6bc8305aab4250f5236ae7eb0d1515f8b779efcc
沐雨 <1912844772@.qqcom>
2022-03-01 18:28:56 +0800

    11

---
+++ src/main/java/com/yutonggang/App.java
@@ -0,0 +1,13 @@
+package com.yutonggang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
